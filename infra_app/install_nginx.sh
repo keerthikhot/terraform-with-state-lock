b8f1a6550e54689cb6c507e9785c59d9671bf59c
keerthikhot <100261542+keerthikhot@users.noreply.github.com>
2026-01-04 13:05:38 +0530

    Delete infra_app directory

--- infra_app/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install
-sudo apt install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-echo "<h1> first project by shrutkirti </h1>" > var/www/html/index.html
--- infra_app/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "terraform-s3-bucket" {
-  bucket = "${var.env}-terraform-s3-bucket"
-
-  tags = {
-    Name        = "${var.env}-terraform-s3-bucket"
-    Environment = "${var.env}-env"
-  }
-}
